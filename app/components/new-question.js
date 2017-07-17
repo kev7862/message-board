7e6be3dec055a8a51019b9f40eb54b2867f2eabe
kev7862 <kevmatite7862@gmail.com>
2017-07-17 16:37:04 +0300

    added question detail.hbs and edited question-tile.js

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -13,7 +13,6 @@ export default Ember.Component.extend({
         content: this.get('content'),
         author: this.get('author'),
         notes: this.get('notes'),
-        date: moment().format('MMMM Do YYYY, h:mm:ss a')
       };
       this.set('addNewQuestion', false);
       this.sendAction('saveQuestion2', params);
